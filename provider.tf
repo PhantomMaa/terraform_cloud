5cc36fd11956f2eacf25f5615982d8acded3b24d
寒玄 <hanxuan.mh@taobao.com>
2021-03-31 20:03:07 +0800

    update

--- provider.tf
+++ provider.tf
@@ -1,12 +1,5 @@
-# The following variable is used to configure the provider's authentication
-# token. You don't need to provide a token on the command line to apply changes,
-# though: using the remote backend, Terraform will execute remotely in Terraform
-# Cloud where your token is already securely stored in your workspace!
-
-variable "provider_token" {
-  type = string
-}
-
-provider "fakewebservices" {
-  token = var.provider_token
+provider "alicloud" {
+  access_key = "LTAI4GG53AunB8Bghm9u6nd3"
+  secret_key = var.provider_token
+  region  = "cn-hangzhou"
 }
